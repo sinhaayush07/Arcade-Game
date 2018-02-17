98100da8774d128b00f312a1a86be316bfc8eba0
Ayush Sinha <toayushofficial@gmail.com>
2018-02-17 19:24:46 +0530

    finished

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Classic Arcade Game
+### How to play
+
+- The player has to avoid collision with the bugs and reach the water to save earth from the wrath of the bugs.
--- js/app.js
+++ js/app.js
@@ -1,19 +1,39 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x,y) {
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.preX = x;
+    this.preY = y;
+
 };
 
-// Update the enemy's position, required method for game
+// Updates the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    // This ensures the game runs at the same speed for
     // all computers.
+    for(let i=0; i<allEnemies.length; i++){
+        allEnemies[i].speed=250;
+    }
+
+    this.x = this.x+(this.speed*dt);
+    
+    //reset the enemy position
+    if(this.x > 500){
+        this.reset();
+    };
+    //handling collision with the enemies
+    if( player.x >= this.x -40 && player.x <=this.x + 40 ){
+        if( player.y >= this.y -40 && player.y <=  this.y+40 ){
+            player.reset();
+        }
+    }
+    //handling if the player reaches the blue line
+    if(player.y <=20){
+        console.log("you won")
+        this.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,19 +41,70 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+Enemy.prototype.reset = function(){
+    this.x = this.preX;
+    this.y = this.preY;
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/**
+ * This is a player class that renders player's image and manages players position
+ */
+let Player = function(x,y){
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y =y;
+    this.preX = x;
+    this.preY = y;
+}
+
+Player.prototype = {
+    update : function(){
+        return true;
+
+    },
+    render : function(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    },
+    handleInput : function(kyprs){
+        if(kyprs === 'left' && this.x > 0){
+            this.x = this.x-50
+        }
+        else if(kyprs === 'right' && this.x < 400){
+            this.x = this.x + 50
+        }
+        else if(kyprs === 'up' && this.y > 0){
+            this.y = this.y - 50
+        }
+        else if(kyprs ==='down' && this.y < 410){
+            this.y = this.y + 50
+        }
+    },
+    reset : function(){
+        this.x = this.preX;
+        this.y = this.preY;
+    }
+}
 
 
+// All enemy objects are in an array called allEnemies
+// The player object is in a variable called player
+var enemy1 = new Enemy(-100,60);
+var enemy4 = new Enemy(-600,60);
+var enemy2 = new Enemy(-165,140);
+var enemy3 = new Enemy(-500,140);
+var enemy5 = new Enemy(-200,220);
+var enemy6 = new Enemy(-400,220);
+var enemy7 = new Enemy(-300,60);
+var enemy8 = new Enemy(-300,140);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+let allEnemies = [enemy1,enemy2,enemy3,enemy4,enemy5,enemy6,enemy7,enemy8];
+
+let player = new Player(0,400)
+
+// This listens for key presses and sends the keys to 
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
